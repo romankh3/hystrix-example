d6f40d0c135e35dc44a59841dd852ffcb4171d6b
= <=>
2018-09-28 10:14:28 +0300

    added repository.

---
+++ src/main/java/com/romankh3/hystrix/Hystrixexample/HystrixExampleApplication.java
@@ -0,0 +1,12 @@
+package com.romankh3.hystrix.Hystrixexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HystrixExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HystrixExampleApplication.class, args);
+	}
+}
